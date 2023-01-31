4ecc0fd81ee3307581019afabf3a54f15ef44d70
Алексей Ануфриенок <alex.anufrienok@gmail.com>
2023-02-01 02:02:59 +0300

    fix header

--- my-cloud/resources/views/includes/header.blade.php
+++ my-cloud/resources/views/includes/header.blade.php
@@ -5,18 +5,22 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 text-secondary">Главная</a></li>
-            <li><a href="/about-us" class="nav-link px-2 text-white">О нас</a></li>
-            <li><a href="/private" class="nav-link px-2 text-white">Личный кабинет</a></li>
+            <li><a href="/" class="{{ request()->is('/') ? 'nav-link px-2 text-secondary' : 'nav-link px-2 text-white' }}">Главная</a></li>
+            <li><a href="/about-us" class="{{ request()->is('about-us') ? 'nav-link px-2 text-secondary' : 'nav-link px-2 text-white' }}">О нас</a></li>
         </ul>
 
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
             <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
         </form>
 
+        @if (Auth::check())
+        <a href="/private" class="{{ request()->is('private') ? 'nav-link px-2 text-secondary' : 'nav-link px-2 text-white' }}">Личный кабинет</a>
+        <a href="/logout" class="btn btn-outline-light me-2">Выйти</a>
+        @else
         <div class="text-end">
             <a href="/login" class="btn btn-outline-light me-2">Вход</a>
             <a href="/registration" class="btn btn-warning">Регистрация</a>
         </div>
+        @endif
     </div>
 </div>
